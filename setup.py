d621895ba02963e246426b1247738125622c5c86
Jay Baird <jay.baird@me.com>
2009-03-23 19:53:47 -0700

    add setuptools and fix setup file

--- setup.py
+++ setup.py
@@ -1,1 +1,45 @@
-from distutils.core import setup
+#!/usr/bin/env python
+from ez_setup import use_setuptools
+use_setuptools()
+
+import os
+
+from setuptools import setup, find_packages, Extension
+
+VERSION = '1.0.0'
+DESCRIPTION = "PyBloom: A Probabilistic data structure"
+LONG_DESCRIPTION = """
+pybloom is a Python implementation of the bloom filter probabilistic data
+structure. The module also provides a Scalable Bloom Filter that allows a 
+bloom filter to grow without knowing the original set size.
+"""
+
+CLASSIFIERS = filter(None, map(str.strip,
+"""                 
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Operating System :: OS Independent
+Topic :: Utilities
+Topic :: Database :: Database Engines/Servers
+Topic :: Software Development :: Libraries :: Python Modules
+""".splitlines()))
+
+FASTBIT_PREFIX = '/mochi/opt/fastbit-ibis1.0.2'
+
+setup(
+    name="pybloom",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    classifiers=CLASSIFIERS,
+    author="Jay Baird",
+    author_email="jay.baird@me.com",
+    url="http://github.com/jaybaird/python-bloomfilter/",
+    license="MIT License",
+    packages=find_packages(exclude=['ez_setup']),
+    platforms=['any'],
+    test_suite="pybloom.tests",
+    zip_safe=True,
+    install_requires=['bitarray >=0.3.4']
+)
